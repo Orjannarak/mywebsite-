720af8dd963db481b32b1322772f24fdca017d77
Orjannarak <97937647+Orjannarak@users.noreply.github.com>
2022-02-09 21:47:11 +0700

    Create table.php

---
+++ table.php
@@ -0,0 +1,53 @@
+<html>
+  <head>
+    <title>ตารางของฉัน</title>
+  </head>
+  <body>
+    <table border="1">
+      <tr>
+        <th>ลำดับ</th>
+        <th>ชื่อ - นามสกุล</th>
+        <th>ชื่อเล่น</th>
+        <th>รูป</th>
+        <th>อินสตราแกรม</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>Payton Moormeier</td>
+        <td>Payton</td>
+        <td><img src="file:///C:/Users/GIGABYTE/Downloads/payton..jpg" width="100px"></td>
+        <td><a href="https://www.instagram.com/paytonmoormeier/?utm_medium=copy_link&fbclid=IwAR1uELGsKqZbTL3SDKl3ZWMElhMzR8FHyFnqNC7CVFAVdhSk1vrUqMD3geo">instagram</a></td>
+      <tr>
+        <td>2</td>
+        <td>Harry Styles</td>
+        <td>Harry</td>
+        <td><img src="file:///C:/Users/GIGABYTE/Downloads/harry.jpg" width="100px"></td>
+        <td><a href="https://www.instagram.com/harrystyles/?fbclid=IwAR1vs9bLJuJ_FkR3CI1RRz3EaBhmUE7I7FTTbS5yVgKFQ6tWedxOYEzHmow">instagram</a></td>
+      <tr>
+        <td>3</td>
+        <td>Charlie Puth</td>
+        <td>Charlie</td>
+        <td><img src="file:///C:/Users/GIGABYTE/Downloads/charlie.jpg" width="100px"></td>
+        <td><a href="https://www.instagram.com/charlieputh/?utm_medium=copy_link&fbclid=IwAR1MKlptFZtr0ZzrBWWnhFIQO0deTYbYbCGPw9WSP-WpP-RQc8hMPIWFlT0">instagram</a></td>
+      <tr>
+        <td>4</td>
+        <td>Christ Hemsworth</td>
+        <td>Christ</td>
+        <td><img src="file:///C:/Users/GIGABYTE/Downloads/christ.jpg" width="100px"></td>
+        <td><a href="https://www.instagram.com/chrishemsworth/?fbclid=IwAR0arId_Shuus9Y2okt5Pe5icizlUdpTrRY54LLj8X-nicb-Hz1qbe9EerE">instagram</a></td>
+      <tr>
+        <td>5</td>
+        <td>Tom Holland</td>
+        <td>Tom Holland</td>
+        <td><img src="file:///C:/Users/GIGABYTE/Downloads/tom.jpg" width="100px"></td>
+        <td><a href="https://www.instagram.com/tomholland2013/?utm_medium=copy_link&fbclid=IwAR3_39WGZ6zg6KNRkIYvYEBCFn_NP3b8opxVE8MaQUcvy-TUv84af4P6hAs">instagram</a></td>
+      <tr>
+        <td>6</td>
+        <td>Robert Downey Jr.</td>
+        <td>Robert Downey</td>
+        <td><img src="file:///C:/Users/GIGABYTE/Downloads/robert.jpg" width="100px"></td>
+        <td><a href="https://www.instagram.com/robertdowneyjr/?utm_medium=copy_link&fbclid=IwAR3nO8-oCUbmOhu9X8lU5ejsFSRpWH6If3mqJ4YSNfRA5tMe_R_Y3coDcAY">instagram</a></td>
+      </tr>
+    </table>
+  </body> 
+</html>
